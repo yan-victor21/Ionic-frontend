66113ef6e08aa571387a4ac56856b4e22cd1caf0
Yan Victor <y.victor66@gmail.com>
2019-04-24 21:01:04 -0300

    Começando a implementar a autenticação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MyApp } from './app.component';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
+import { AuthService } from '../services/auth.service';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoriaService,
-    ErrorInterceptorProvider
+    ErrorInterceptorProvider,
+    AuthService
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { CredenciaisDTO } from './../../models/credenciais.dto';
 import { Component } from '@angular/core';
 import { NavController,IonicPage,  } from 'ionic-angular';
 import { MenuController } from 'ionic-angular/components/app/menu-controller';
+import { AuthService } from '../../services/auth.service';
 
 @IonicPage()
 @Component({
@@ -15,7 +16,9 @@ export class HomePage {
     senha : ""
   };
 
-  constructor(public navCtrl: NavController, public menu: MenuController) {}
+  constructor(public navCtrl: NavController, 
+              public menu: MenuController,
+              public auth: AuthService) {}
   
   ionViewWillEnter() {
     this.menu.swipeEnable(false);
@@ -26,7 +29,13 @@ export class HomePage {
   }
 
   login(){
-    console.log(this.creds);
-    this.navCtrl.setRoot('CategoriasPage');//Abre a pagina de categorias
+    this.auth.authenticate(this.creds).subscribe(response =>{
+      console.log(response.headers.get('Authorization'));
+      this.navCtrl.setRoot('CategoriasPage');//Abre a pagina de categorias
+    },err =>{
+      
+    });
+    
+    
   }
 }
