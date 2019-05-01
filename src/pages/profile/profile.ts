49fa56cfec52fc301faae68c08b511516c667ba2
Yan Victor <y.victor66@gmail.com>
2019-04-30 21:05:41 -0300

    Buscando endereços do banco de dados

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -21,7 +21,7 @@ export class ProfilePage {
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email){
       this.clienteService.findByEmail(localUser.email).subscribe(response =>{
-        this.cliente = response;
+        this.cliente = response as ClienteDTO;
         this.getImageIfExists();
       }, error =>{
         if(error.status == 403){
