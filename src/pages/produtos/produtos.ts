1f8f84f3dc3f8e6bbaa913bb0150f8dd7ee6e555
Yan Victor <y.victor66@gmail.com>
2019-05-02 15:11:27 -0300

    Infinit Scrol

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -3,13 +3,6 @@ import { ProdutoDTO } from './../../models/produto.dto';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 
-/**
- * Generated class for the ProdutosPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-produtos',
@@ -17,7 +10,8 @@ import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-an
 })
 export class ProdutosPage {
 
-  items: ProdutoDTO[];
+  items: ProdutoDTO[] =[];
+  page:number=0;
   constructor(
               public navCtrl: NavController, 
               public navParams: NavParams,
@@ -28,10 +22,12 @@ export class ProdutosPage {
   loadData(){
     let categoria_id = this.navParams.get('categoria_id')
     let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoria_id).subscribe(response =>{
-      this.items = response['content'];
+    this.produtoService.findByCategoria(categoria_id,this.page,10).subscribe(response =>{
+      let start = this.items.length;
+      this.items = this.items.concat(response['content']);
+      let end = this.items.length-1;
       loader.dismiss();
-      this.loadImageUrls();
+      this.loadImageUrls(start,end);
     },error =>{
       loader.dismiss();
     });
@@ -40,8 +36,8 @@ export class ProdutosPage {
     this.loadData();
   }
 
-  loadImageUrls() {
-    for (var i=0; i<this.items.length; i++) {
+  loadImageUrls(start: number, end: number) {
+    for (var i= start; i<= end; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImage(item.id)
         .subscribe(response => {
@@ -64,9 +60,19 @@ export class ProdutosPage {
   }
 
   doRefresh(refresher) {
+    this.page = 0;
+    this.items = [];
     this.loadData();
     setTimeout(() => {
       refresher.complete();
+    }, 1000);
+  }
+
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.loadData();
+    setTimeout(() => {
+      infiniteScroll.complete();
     }, 1000);
   }
 }
